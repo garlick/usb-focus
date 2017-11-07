944f9850709af5f54b85e79057b40ec1b4f5433e
Jim Garlick <garlick.jim@gmail.com>
2017-11-06 21:21:20 -0800

    firmware: add "cmdport" for usb serial

---
+++ firmware/cmdport.h
@@ -0,0 +1,7 @@
+void cmdport_setup (void);
+void cmdport_finalize (void);
+void cmdport_update (void);
+
+/*
+ * vi:tabstop=4 shiftwidth=4 expandtab
+ */
--- firmware/main.c
+++ firmware/main.c
@@ -1,17 +1,21 @@
 #include <WProgram.h>
 #include "stepper.h"
 #include "buttons.h"
+#include "cmdport.h"
 
 int main (void)
 {
     stepper_setup ();
     buttons_setup ();
+    cmdport_setup ();
 
     while (1) {
         stepper_update ();
         buttons_update ();
+        cmdport_update ();
     }
 
+    cmdport_finalize ();
     buttons_finalize ();
     stepper_finalize ();
 }
