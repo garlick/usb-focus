e90c10c19ee25c262ebc34b601c06f724d6d03ea
Jim Garlick <garlick.jim@gmail.com>
2017-11-12 07:56:29 -0800

    firmware: add stepper_stop, _move_in, _move_out()

--- firmware/stepper.h
+++ firmware/stepper.h
@@ -8,6 +8,16 @@ EXTERNC void stepper_setup (void);
 EXTERNC void stepper_finalize (void);
 EXTERNC void stepper_update (void);
 
+/* Stop (quickly)
+ */
+EXTERNC void stepper_stop (void);
+
+/* Move focal plane towards telescope (in) or camera (out), with acceleration.
+ * Stop wtih stepper_stop().
+ */
+EXTERNC void stepper_move_in (void);
+EXTERNC void stepper_move_out (void);
+
 /*
  * vi:tabstop=4 shiftwidth=4 expandtab
  */
