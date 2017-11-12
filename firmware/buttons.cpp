57bc0bcd1feea731f2af0189fd6e80496c754dc5
Jim Garlick <garlick.jim@gmail.com>
2017-11-11 19:59:18 -0800

    firmware: enable pullups on buttons, cleanup

--- firmware/buttons.cpp
+++ firmware/buttons.cpp
@@ -5,17 +5,29 @@
 const uint8_t in_button_pin = 14;
 const uint8_t out_button_pin = 15;
 
-static Bounce out_button = Bounce (out_button_pin, 5);
-static Bounce in_button = Bounce (in_button_pin, 5);
+static Bounce in = Bounce (in_button_pin, 5);
+static Bounce out = Bounce (out_button_pin, 5);
 
 void buttons_setup (void)
 {
-    pinMode (out_button_pin, INPUT);
-    pinMode (in_button_pin, INPUT);
+    pinMode (in_button_pin, INPUT_PULLUP);
+    pinMode (out_button_pin, INPUT_PULLUP);
 }
 
 void buttons_update (void)
 {
+    in.update ();
+    if (in.risingEdge ())
+        Serial.printf ("in button (rising)\n");
+    if (in.fallingEdge ())
+        Serial.printf ("in button (falling)\n");
+
+    out.update ();
+    if (out.risingEdge ())
+        Serial.printf ("out button (rising)\n");
+    if (out.fallingEdge ())
+        Serial.printf ("out button (falling)\n");
+
 }
 
 void buttons_finalize (void)
